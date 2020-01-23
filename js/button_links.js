157c5ff695195a535b7ac7d9db16f64a230a11f0
Guthen <33220603+Guthen@users.noreply.github.com>
2020-01-23 16:51:34 +0100

    added more contributors

--- js/button_links.js
+++ js/button_links.js
@@ -2,10 +2,22 @@ const button_links = new Vue( {
     el: ".main",
     data: {
         buttons: [
-            { title: "Accueil", link: "index.html" },
-            { title: "Wiki", link: "wiki.html" },
-            { title: "Github", link: "https://github.com/Nogitsu/GNLib" },
-            { title: "Discord", link: "https://discord.gg/U425mC" },
+            { 
+                title: "Accueil", 
+                link: "index.html" 
+            },
+            { 
+                title: "Wiki", 
+                link: "wiki.html" 
+            },
+            { 
+                title: "Github", 
+                link: "https://github.com/Nogitsu/GNLib"
+            },
+            { 
+                title: "Discord", 
+                link: "https://discord.gg/U425mC" 
+            },
         ]
     }
 } )
--- js/list_creators.js
+++ js/list_creators.js
@@ -18,6 +18,16 @@ const list_creators = new Vue( {
                 role: "Website Creator",
                 image: "https://cdn.discordapp.com/avatars/324645432493867008/4398f438e6307fd58f3e5182905d8897.webp?size=256",
             },
+            {
+                name: "Fleoxi",
+                role: "Archivist",
+                image: "https://avatars2.githubusercontent.com/u/43684766?s=460&v=4",
+            },
+            {
+                name: "GCA",
+                role: "Sponsor",
+                image: "https://cdn.discordapp.com/icons/223070469148901376/f184858bafe8e8cc328e613944f26c5b.png?size=256"
+            }
         ]
     }
 } )
