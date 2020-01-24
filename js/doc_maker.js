8ee70a9c166f2c7b8f489452e4d98177748755c5
Guthen <33220603+Guthen@users.noreply.github.com>
2020-01-24 22:17:28 +0100

    docmaker, vuejs operationnal

--- js/doc_maker.js
+++ js/doc_maker.js
@@ -3,8 +3,16 @@ const docmaker = new Vue( {
     el: ".docmaker",
     data: {
         args: {
-            title: "Title",
-            note: "",
+            title: { value: "", is_var: true },
+            note: { value: "" },
+            params: { value: [], is_var: true },
+            example: { 
+                hyper_text: {
+                    prompt: "",
+                    code: "",
+                    output: "",
+                } 
+            }
         },
     },
     computed: {
@@ -13,13 +21,26 @@ const docmaker = new Vue( {
 
             for ( const k in this.args ) {
                 const v = this.args[k]
-                if ( v === "" ) continue
+                if ( v.value === "" || ( v.value instanceof Array && v.value.length === 0 ) || ( v.hyper_text && ( !v.hyper_text.prompt && !v.hyper_text.code && !v.hyper_text.output ) ) ) continue
                 
                 output += `--- @${k}:\n`
-                output += `--- ${v}: <> ...\n`
-            }
+                if ( v.is_var ) {
+                    if ( v.value instanceof Array ) {
+                        v.value.forEach( _v => {
+                            output += `--- \t${_v.value}: <${_v.type}> ${_v.desc}\n`
+                        } );
+                    }
+                    else output += `--- \t${v.value}: <${v.type}> ${v.desc}\n`
+                }
+                else if ( v.hyper_text ) {
+                    const code = v.hyper_text.code.replace( /\n/g, "\\n" ).replace( /\s\s\s\s/g, "\\t" )
 
-            console.log(output);
+                    output += `--- \t#prompt: ${v.hyper_text.prompt}\n`
+                    output += `--- \t#code: ${code}\n`
+                    output += `--- \t#output: ${v.hyper_text.output}\n`
+                }
+                else output += `--- \t${v.value}\n`              
+            }
             
             return output
         },
