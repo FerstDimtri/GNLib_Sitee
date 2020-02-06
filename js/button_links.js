15679889b57749d5aaffa0ac2d2f6bfbe67408c3
Guthen <33220603+Guthen@users.noreply.github.com>
2020-02-06 16:16:29 +0100

    buttons system rework

--- js/button_links.js
+++ js/button_links.js
@@ -1,23 +1,12 @@
 const button_links = new Vue( {
     el: ".main",
     data: {
-        buttons: [
-            { 
-                title: "Accueil", 
-                link: "index.html" 
-            },
-            { 
-                title: "Wiki", 
-                link: "wiki.html" 
-            },
-            { 
-                title: "Github", 
-                link: "https://github.com/Nogitsu/GNLib"
-            },
-            { 
-                title: "Discord", 
-                link: "https://discord.gg/U425mC" 
-            },
-        ]
+        buttons: {
+            ["Accueil"]: "#",
+            ["Wiki"]: "wiki/index.html",
+            ["Docmaker"]: "docmaker.html",
+            ["Github"]: "https://github.com/Nogitsu/GNLib",
+            ["Discord"]: "https://discord.gg/U425mC",
+        }
     }
 } )
