da6cd1cadcb9ee9f965a7e57f56562414183ad1c
Guthen <33220603+Guthen@users.noreply.github.com>
2020-02-17 11:47:03 +0100

    added multiple links support for nav categories

--- js/button_links.js
+++ js/button_links.js
@@ -1,6 +1,7 @@
 const button_links = new Vue( {
     el: ".main",
     data: {
+<<<<<<< Updated upstream
         buttons: [
             { 
                 title: "Accueil", 
@@ -19,5 +20,14 @@ const button_links = new Vue( {
                 link: "https://discord.gg/U425mC" 
             },
         ]
+=======
+        buttons: {
+            ["Accueil"]: "#",
+            ["Wiki"]: "wiki/index.html",
+            ["Docmaker"]: "docmaker.html",
+            ["Github"]: "https://github.com/Nogitsu/GNLib",
+            ["Discord"]: "https://discord.gg/U425mC",
+        }
+>>>>>>> Stashed changes
     }
 } )
